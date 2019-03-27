1e71a0c4ad41cd5babc15984d881a01569c3db15
GJonhnsonII <46767662+GJonhnsonII@users.noreply.github.com>
2019-03-26 23:00:17 -0400

    Revised Homework

--- Week 4/Program2/program2.cpp
+++ Week 4/Program2/program2.cpp
@@ -5,23 +5,29 @@ using namespace std;
 int main() {
 	int x = 0;
 	int count = 0;
+	int message;
 	int const rows = 30;
 	int const col = 100;
 	char students[rows][col];
 	for (x = 0; x < rows; x++) {
-		strcpy_sv
+		strcpy_s
 		(students[x], "");
 	}
 	for (x = 0; x < rows; x++) {
-		cout << "Please enter student for index " << x << " :";
+		cout << "Please enter student information: ";
 		cin.getline(students[x], col);
 		if (!strcmp("0", students[x])) {
-			cout << "Got a 0, breaking." << endl;
+			cout << "0 entered, end of list" << endl;
 			break;
 		}
 		count++;
 	}
 	for (x = 0; x < count; x++) {
-		cout << "students[" << x << "] = " << students[x] << endl;
+		cout << "student " << x << " : " << students[x] << endl;
 	}
+	for (x = count ; x < count + 1 ; x++) {
+		cout << "end of list; Enter any key and press enter to exit.";
+		cin >> message;
+	}
+	return 0;
 }
